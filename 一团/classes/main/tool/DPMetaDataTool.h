9b8a0ebdb77b1b8f183ecfb5d7c30a2b03ae3def
SoXeon <dpsopower@gmail.com>
2014-05-16 00:18:14 +0800

    对外提供一个当前选中的城市，设置分类、排序数据

--- 一团/classes/main/tool/DPMetaDataTool.h
+++ 一团/classes/main/tool/DPMetaDataTool.h
@@ -14,5 +14,8 @@
 singleton_interface(DPMetaDataTool)
 @property (nonatomic,strong,readonly) NSArray *totalCitySections;//所有城市组数据
 @property (nonatomic,strong,readonly) NSDictionary *totalCities;//所有城市
-@property (nonatomic,strong) DPCity *currentCity;
+@property (nonatomic,strong) DPCity *currentCity;//当前选中的城市
+@property (nonatomic,strong,readonly) NSArray *totalCategories;//所有的分类数据
+//所有的排序数据
+@property (nonatomic,strong,readonly)NSArray *totalOrders;
 @end

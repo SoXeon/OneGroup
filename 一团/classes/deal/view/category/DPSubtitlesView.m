aa59779fcda5ef1ef31323e74e58a6fb8727a4cf
SoXeon <dpsopower@gmail.com>
2014-05-18 14:58:52 +0800

    对小按钮对监听，在不同页面的显示过程中，将页面布局实现动态伸缩

--- 一团/classes/deal/view/category/DPSubtitlesView.m
+++ 一团/classes/deal/view/category/DPSubtitlesView.m
@@ -8,11 +8,17 @@
 
 #import "DPSubtitlesView.h"
 #import "UIImage+DP.h"
+#import "DPMetaDataTool.h"
 
 #define kTitleW 100
 #define kTitleH 40
 
-#define kDuration 1.0
+@interface DPSubtitlesView()
+{
+    UIButton *_selectedBtn;
+}
+
+@end
 
 @implementation DPSubtitlesView
 
@@ -22,8 +28,30 @@
     if (self) {
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         self.image = [UIImage resizedImage:@"bg_subfilter_other.png"];
+        //裁剪超出父控件的子控件
+        self.clipsToBounds = YES;
+        self.userInteractionEnabled = YES;
     }
     return self;
+}
+
+#pragma mark 监听小按钮的点击
+- (void)titleClick:(UIButton *)btn
+{
+    //控制按钮状态
+    _selectedBtn.selected = NO;
+    btn.selected = YES;
+    _selectedBtn = btn;
+    
+    //设置当前选中的分类文字
+//    if (_setTitleBlock) {
+//        _setTitleBlock([btn titleForState:UIControlStateNormal]);
+//    }
+    if ([_delegate respondsToSelector:@selector(subtitlesView:titleClick:)]) {
+        [_delegate subtitlesView:self titleClick:[btn titleForState:UIControlStateNormal]];
+    }
+    
+    
 }
 
 - (void)setTitles:(NSArray *)titles
@@ -37,7 +65,9 @@
         UIButton *btn = nil;
         if (i >= self.subviews.count) { // 按钮个数不够
             btn = [UIButton buttonWithType:UIButtonTypeCustom];
+            [btn addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
             [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+            [btn setBackgroundImage:[UIImage resizedImage:@"slider_filter_bg_active.png"] forState:UIControlStateSelected];
             [self addSubview:btn];
         } else {
             btn = self.subviews[i];
@@ -46,6 +76,24 @@
         // 2.设置按钮文字
         btn.hidden = NO;
         [btn setTitle:titles[i] forState:UIControlStateNormal];
+        
+        //3.根据按钮文字来设定是否被选中
+        if ([_delegate respondsToSelector:@selector(subtitlesViewGetCurrentTitle:)]) {
+            NSString *current =  [_delegate subtitlesViewGetCurrentTitle:self];
+            btn.selected = [titles[i] isEqualToString:current];
+            if (btn.selected) {
+                _selectedBtn = btn;
+            }
+        } else {
+                btn.selected = NO;
+            }
+        
+//        if (_getTitleBlock) {
+//            NSString *current = _getTitleBlock();
+//            btn.selected = [titles[i] isEqualToString:current];
+//        } else {
+//            btn.selected = NO;
+//        }
     }
     
     // 隐藏后面多余的按钮
@@ -53,6 +101,7 @@
         UIButton *btn = self.subviews[i];
         btn.hidden = YES;
     }
+    [self layoutSubviews];
 }
 
 // 控件本身的宽高发生改变的时候就会调用
@@ -71,12 +120,10 @@
         btn.frame = CGRectMake(x, y, kTitleW, kTitleH);
     }
     
-    //    [UIView animateWithDuration:1.0 animations:^{
-    int rows = (_titles.count + columns - 1) / columns;
-    CGRect frame = self.frame;
-    frame.size.height = rows * kTitleH;
-    self.frame = frame;
-    //    }];
+        int rows = (_titles.count + columns - 1) / columns;
+        CGRect frame = self.frame;
+        frame.size.height = rows * kTitleH;
+        self.frame = frame;
 }
 
 - (void)show
@@ -85,7 +132,7 @@
     
     self.transform = CGAffineTransformMakeTranslation(0, -self.frame.size.height);
     self.alpha = 0;
-    [UIView animateWithDuration:kDuration animations:^{
+    [UIView animateWithDuration:kDefaultAnimDuration animations:^{
         self.transform = CGAffineTransformIdentity;
         self.alpha = 1;
     }];
@@ -93,13 +140,20 @@
 
 - (void)hide
 {
-    [UIView animateWithDuration:kDuration animations:^{
+    [UIView animateWithDuration:kDefaultAnimDuration animations:^{
         self.transform = CGAffineTransformMakeTranslation(0, -self.frame.size.height);
         self.alpha = 0;
     } completion:^(BOOL finished) {
+        [self removeFromSuperview];
+        
+        CGRect f = self.frame;
+        f.size.height = 0;
+        self.frame = f;
+        
         self.transform = CGAffineTransformIdentity;
         self.alpha = 1;
     }];
 }
+
 
 @end

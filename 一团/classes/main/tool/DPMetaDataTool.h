20df70b4d0b611cef182ee39a100e6dad0d7200e
SoXeon <dpsopower@gmail.com>
2014-05-18 14:51:53 +0800

    添加当前选中的城市

--- 一团/classes/main/tool/DPMetaDataTool.h
+++ 一团/classes/main/tool/DPMetaDataTool.h
@@ -8,14 +8,22 @@
 
 #import <Foundation/Foundation.h>
 #import "Singleton.h"
-@class DPCity;
+
+@class DPCity,DPOrder;
 
 @interface DPMetaDataTool : NSObject
 singleton_interface(DPMetaDataTool)
 @property (nonatomic,strong,readonly) NSArray *totalCitySections;//所有城市组数据
 @property (nonatomic,strong,readonly) NSDictionary *totalCities;//所有城市
-@property (nonatomic,strong) DPCity *currentCity;//当前选中的城市
+
 @property (nonatomic,strong,readonly) NSArray *totalCategories;//所有的分类数据
 //所有的排序数据
 @property (nonatomic,strong,readonly)NSArray *totalOrders;
+
+-(DPOrder *)orderWithName:(NSString *)name;
+
+@property (nonatomic,strong) DPCity *currentCity;//当前选中的城市
+@property (nonatomic,strong) NSString *currentCategory;//当前选中的分类
+@property (nonatomic,strong) NSString *currentDistrict;//当前选中的区域
+@property (nonatomic,strong) DPOrder *currentOrder;//当前选中的排序
 @end

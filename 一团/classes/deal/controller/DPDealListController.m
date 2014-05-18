8e99ac04231ba7a7d07e1a90828f7066658cbe33
SoXeon <dpsopower@gmail.com>
2014-05-18 14:56:59 +0800

    添加大众点评的相关数据调用，实现传输不同城市的不同团购情况，获取了真实的数据，将监听通知写到Common.h中，设置了当选取城市当时候向服务器发送当请求，及获取内容，并刷新

--- 一团/classes/deal/controller/DPDealListController.m
+++ 一团/classes/deal/controller/DPDealListController.m
@@ -8,19 +8,36 @@
 
 #import "DPDealListController.h"
 #import "DPDealTopMenu.h"
+#import "DPAPI.h"
+#import "DPMetaDataTool.h"
+#import "DPCity.h"
+#import "DPDeal.h"
+#import "NSObject+Value.h"
 
-@interface DPDealListController ()
+@interface DPDealListController () <DPRequestDelegate>
+{
+    NSMutableArray *_deals;
+}
 
 @end
 
 @implementation DPDealListController
+
+- (id)init
+{
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+    layout.itemSize = CGSizeMake(250, 250);
+    layout.minimumLineSpacing = 30;
+    layout.sectionInset = UIEdgeInsetsMake(35, 35, 35, 35);
+    return [super initWithCollectionViewLayout:layout];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
     //监听城市改变的通知
-    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(cityChange) name:kCityChangeNote object:nil];
+    kAddAllNotes(dataChange)
     
     //设置背景颜色
     self.view.backgroundColor = kGlobalBg;
@@ -35,11 +52,37 @@
     DPDealTopMenu *top = [[DPDealTopMenu alloc] init];
     top.contentView = self.view;
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:top];
+    
+    //注册cell的xib
+    [self.collectionView registerNib:[UINib nibWithNibName:@"DealCell" bundle:nil] forCellWithReuseIdentifier:@"deal"];
 }
 
--(void)cityChange
+-(void)dataChange
 {
-    MyLog(@"城市发生改变");
+    
+    DPAPI *api = [[DPAPI alloc]init];
+    
+    [api requestWithURL:@"v1/deal/find_deals"
+                 params:@{
+                          @"city":[DPMetaDataTool sharedDPMetaDataTool].currentCity.name
+                        } delegate:self];
+    
+}
+
+- (void)request:(DPRequest *)request didFinishLoadingWithResult:(id)result
+{
+
+    NSArray *array = result[@"deals"];
+    
+    _deals = [NSMutableArray array];
+    
+    for (NSDictionary *dict in array) {
+        DPDeal *d = [[DPDeal alloc]init];
+        [d setValues:dict];
+        [_deals addObject:d];
+    }
+    
+    [self.collectionView reloadData];
 }
 
 -(void)dealloc
@@ -47,4 +90,17 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ID = @"deal";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ID forIndexPath:indexPath];
+   
+    return cell;
+}
 @end

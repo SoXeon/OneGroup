c552f1f5f1f0247996c89d923db100ef50d5d58a
SoXeon <dpsopower@gmail.com>
2014-05-18 14:57:37 +0800

    实现了子标题控制协议及时时显示标题

--- 一团/classes/deal/view/category/DPSubtitlesView.h
+++ 一团/classes/deal/view/category/DPSubtitlesView.h
@@ -7,10 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+@class DPSubtitlesView;
+
+@protocol DPSubtitlesViewDelegate <NSObject>
+@optional
+-(void)subtitlesView:(DPSubtitlesView *)subtitlesView titleClick:(NSString *)title;
+-(NSString *)subtitlesViewGetCurrentTitle:(DPSubtitlesView *)subtitlesView;
+@end
 
 @interface DPSubtitlesView : UIImageView
 
 @property (nonatomic, strong) NSArray *titles; // 所有的子标题文字
+
+@property (nonatomic,weak) id<DPSubtitlesViewDelegate> delegate;
+
+//@property (nonatomic,copy) void (^setTitleBlock)(NSString *title);
+//
+//@property (nonatomic,copy) NSString *(^getTitleBlock)();
 
 // 通过动画显示出来
 - (void)show;

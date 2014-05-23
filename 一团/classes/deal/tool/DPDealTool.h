5d9a7b800d087ca2e83aa455bab38fb3e7422fc5
SoXeon <dpsopower@gmail.com>
2014-05-23 20:03:24 +0800

    向大众点评服务器请求，团购详情的数据

--- 一团/classes/deal/tool/DPDealTool.h
+++ 一团/classes/deal/tool/DPDealTool.h
@@ -9,12 +9,20 @@
 #import <Foundation/Foundation.h>
 #import "Singleton.h"
 
+@class DPDeal;
+
 typedef void (^DealsSuccessBlock)(NSArray *deals,int totalCount);
 typedef void (^DealsErrorBlock)(NSError *error);
+
+typedef void (^DealSuccessBlock)(DPDeal *deal);
+typedef void (^DealErrorBlock)(NSError *error);
 
 
 @interface DPDealTool : NSObject
 singleton_interface(DPDealTool)
 - (void)dealsWithPage:(int)page success:(DealsSuccessBlock)success error:(DealsErrorBlock)error;
 
+
+#pragma mark 获取指定团购详情数据
+-(void)dealWithID:(NSString *)ID success:(DealSuccessBlock)success error:(DealErrorBlock)error;
 @end

efab73403a27b1e5b34dffd7ce05608a3e745a51
SoXeon <dpsopower@gmail.com>
2014-05-23 20:00:17 +0800

    添加了团购详情的控制器，设置上拉加载，下拉刷新，及清理图片缓存，封装了一些基本设置，

--- 一团/classes/deal/controller/DPDealListController.m
+++ 一团/classes/deal/controller/DPDealListController.m
@@ -17,9 +17,17 @@
 #import "DPDealTool.h"
 #import "MJRefresh.h"
 #import "DPImageTool.h"
+#import "UIBarButtonItem+DP.h"
+
+#import "DPDealDetailController.h"
+#import "DPNavigationController.h"
+
+#import "DPCover.h"
 
 #define kItemW 250
 #define kItemH 250
+
+#define kDetailWitdh 600
 
 
 @interface DPDealListController () <DPRequestDelegate,MJRefreshBaseViewDelegate>
@@ -28,6 +36,8 @@
     int _page;//页码
     MJRefreshFooterView *_footer;//下拉加载更多
     MJRefreshHeaderView *_header;
+    
+    DPCover *_cover;
 }
 
 @end
@@ -37,30 +47,87 @@
 - (id)init
 {
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
-    layout.minimumLineSpacing = 30;
     layout.itemSize = CGSizeMake(kItemW, kItemH);
-    return [super initWithCollectionViewLayout:layout];
-
+    layout.minimumLineSpacing = 20;
+    return [self initWithCollectionViewLayout:layout];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-    //监听城市改变的通知
-    kAddAllNotes(dataChange)
-    
     //基本设置
     [self baseSetting];
     
     //添加刷新控件
     [self addRefresh];
+    
+    [DPMetaDataTool sharedDPMetaDataTool].currentCity = [DPMetaDataTool sharedDPMetaDataTool].totalCities[@"上海"];
+    
+}
+
+#pragma mark 下拉刷新，上拉加载更多
+-(void)addRefresh
+{
+    
+    NSComparisonResult order = [[UIDevice currentDevice].systemVersion compare: @"7.0" options: NSNumericSearch];
+    if (order == NSOrderedSame || order == NSOrderedDescending)
+    {
+         //OS version >= 7.0
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+   }
+    
+    MJRefreshHeaderView *header = [MJRefreshHeaderView header];
+    header.scrollView = self.collectionView;
+    header.delegate = self;
+    _header = header;
+    
+    MJRefreshFooterView *footer = [MJRefreshFooterView footer];
+    footer.scrollView = self.collectionView;
+    footer.delegate = self;
+    _footer = footer;
+}
+
+#pragma mark 刷新代理方法
+- (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView
+{
+    BOOL isHeader = [refreshView isKindOfClass:[MJRefreshFooterView class]];
+    if (isHeader) {
+        //清除图片缓存
+        [DPImageTool clear];
+        _page = 1;
+    }  else {
+        //上拉加载
+        _page++;
+    }
+    
+    [[DPDealTool sharedDPDealTool] dealsWithPage:_page success:^(NSArray *deals, int totalCount) {
+        if (isHeader) {
+            _deals = [NSMutableArray array];
+        }
+        //添加数据
+        [_deals addObjectsFromArray:deals];
+        //刷新表格
+        [self.collectionView reloadData];
+        //恢复刷新状态
+        [refreshView endRefreshing];
+        //判断下拉加载更多是否要刷新
+        _footer.hidden = _deals.count >= totalCount;
+    } error:^(NSError *error){
+        //恢复刷新状态
+        [refreshView endRefreshing];
+    }];
 }
 
 - (void)baseSetting
 {
+    //监听城市改变的通知
+    kAddAllNotes(dataChange)
+    
     //设置背景颜色
     self.collectionView.backgroundColor = kGlobalBg;
+    [self.collectionView setDelegate:self];
+    [self.collectionView setDataSource:self];
     
     //右边搜索框
     UISearchBar *s = [[UISearchBar alloc]init];
@@ -78,60 +145,7 @@
     
     //设置collectionView永远支持垂直滚动
     self.collectionView.alwaysBounceVertical = YES;
-
-}
-
-#pragma mark 下拉刷新，上拉加载更多
--(void)addRefresh
-{
     
-    NSComparisonResult order = [[UIDevice currentDevice].systemVersion compare: @"7.0" options: NSNumericSearch];
-    if (order == NSOrderedSame || order == NSOrderedDescending)
-    {
-        // OS version >= 7.0
-        self.edgesForExtendedLayout = UIRectEdgeNone;
-    }
-    
-    MJRefreshHeaderView *header = [MJRefreshHeaderView header];
-    header.scrollView = self.collectionView;
-    header.delegate = self;
-    _header = header;
-    
-    MJRefreshFooterView *footer = [MJRefreshFooterView footer];
-    footer.scrollView = self.collectionView;
-    footer.delegate = self;
-    _footer = footer;
-}
-
-#pragma mark 刷新代理方法
-- (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView
-{
-    if ([refreshView isKindOfClass:[MJRefreshFooterView class]]) {
-        //清除图片缓存
-        [DPImageTool clear];
-        
-        //下拉刷新
-        _deals = [NSMutableArray array];
-        _page = 1;
-    }  else {
-        //上拉加载
-        _page++;
-    }
-    
-    [[DPDealTool sharedDPDealTool] dealsWithPage:_page success:^(NSArray *deals, int totalCount) {
-        //添加数据
-        [_deals addObjectsFromArray:deals];
-        //刷新表格
-        [self.collectionView reloadData];
-        //恢复刷新状态
-        [refreshView endRefreshing];
-        //判断下拉加载更多是否要刷新
-        _footer.hidden = _deals.count >= totalCount;
-    } error:^(NSError *error){
-        //恢复刷新状态
-        [refreshView endRefreshing];
-
-    }];
 }
 
 -(void)viewWillAppear:(BOOL)animated
@@ -153,13 +167,7 @@
     } error:nil];
 }
 
-
 #pragma mark 屏幕旋转的时候调用
-- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
-{
-    
-}
-
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
 {
     //取出layout
@@ -178,7 +186,6 @@
         layout.sectionInset = UIEdgeInsetsMake(v, h, v, h);
     }];
 }
-
 
 -(void)dealloc
 {
@@ -199,5 +206,57 @@
     cell.deal = _deals[indexPath.row];
    
     return cell;
+}
+
+#pragma mark -代理方法
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self showDetail:_deals[indexPath.row]];
+}
+
+#pragma mark 显示详情控制器
+-(void)showDetail:(DPDeal *)deal
+{
+    if (_cover == nil) {
+        _cover = [DPCover coverWithTarget:self action:@selector(hideDetail)];}
+    _cover.frame = self.navigationController.view.bounds;
+    _cover.alpha = 0;
+    [UIView animateWithDuration:0.4 animations:^{
+        [_cover reset];
+    }];
+    [self.navigationController.view addSubview:_cover];
+    
+    //展示团购详情
+    DPDealDetailController *detail = [[DPDealDetailController alloc]init];
+    detail.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithIcon:@"btn_nac_close.png" highlightedIcon:@"btn_nav_close_hl.png" target:self action:@selector(hideDetail)];
+    detail.deal = deal;
+    DPNavigationController *nav = [[DPNavigationController alloc]initWithRootViewController:detail];
+    nav.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleLeftMargin;
+    nav.view.frame = CGRectMake(_cover.frame.size.width, 0, 600, _cover.frame.size.height);
+    //两个控制器为互为父子关系，view也是一样的 
+    [self.navigationController.view addSubview:nav.view];
+    [self.navigationController addChildViewController:nav];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = nav.view.frame;
+        f.origin.x -= kDetailWitdh;
+        nav.view.frame = f;
+    }];
+}
+
+-(void)hideDetail
+{
+    //隐藏遮盖
+    UIViewController *nav = [self.navigationController.childViewControllers lastObject];
+    [UIView animateWithDuration:0.3 animations:^{
+        _cover.alpha = 0;
+        CGRect f = nav.view.frame;
+        f.origin.x += kDetailWitdh;
+        nav.view.frame = f;
+    } completion:^(BOOL finished) {
+        [_cover removeFromSuperview];
+        [nav.view removeFromSuperview];
+        [nav removeFromParentViewController];
+    }];
 }
 @end

cdd2c0ba3ae8e4cb42cdd78684ec2dd3b1301d28
SoXeon <dpsopower@gmail.com>
2014-05-23 20:02:44 +0800

    设置当前价格，原来价格

--- 一团/classes/deal/model/DPDeal.m
+++ 一团/classes/deal/model/DPDeal.m
@@ -7,6 +7,35 @@
 //
 
 #import "DPDeal.h"
+#import "DPRestrictions.h"
+#import "NSString+DP.h"
+#import "NSObject+Value.h"
 
 @implementation DPDeal
+
+- (void)setList_price:(double)list_price
+{
+    _list_price = list_price;
+    
+    _list_price_text = [NSString stringWithDouble:list_price fractionCount:2];
+}
+
+-(void)setCurrent_price:(double)current_price
+{
+    _current_price = current_price;
+    
+    _current_price_text = [NSString stringWithDouble:current_price fractionCount:2];
+
+}
+
+-(void)setRestrictions:(DPRestrictions *)restrictions
+{
+    if ([restrictions isKindOfClass:[NSDictionary class]]) {
+        _restrictions = [[DPRestrictions alloc] init];
+        [_restrictions setValues:(NSDictionary *)restrictions];
+    } else {
+        _restrictions = restrictions;
+    }
+}
+
 @end

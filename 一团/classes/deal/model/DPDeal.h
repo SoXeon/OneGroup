a4b54815922aea541f72b98b69bedf2e86fcba9f
SoXeon <dpsopower@gmail.com>
2014-05-23 20:02:01 +0800

    添加团购详情中，按钮的选中，及详细描述的属性

--- 一团/classes/deal/model/DPDeal.h
+++ 一团/classes/deal/model/DPDeal.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013年 itcast. All rights reserved.
 //  一个团购模型
 
+@class DPRestrictions;
+
 #import <Foundation/Foundation.h>
 @interface DPDeal : NSObject
 @property (nonatomic, copy) NSString *deal_id; // 团购ID
@@ -14,8 +16,8 @@
 @property (nonatomic, assign) double list_price;
 @property (nonatomic, assign) double current_price; // 当前价格
 
-//@property (nonatomic, readonly) NSString *list_price_text;
-//@property (nonatomic, readonly) NSString *current_price_text; // 当前价格
+@property (nonatomic, readonly) NSString *list_price_text;
+@property (nonatomic, readonly) NSString *current_price_text; // 当前价格
 
 @property (nonatomic, strong) NSArray *regions; // 区域
 @property (nonatomic, strong) NSArray *categories; // 分类
@@ -25,4 +27,9 @@
 @property (nonatomic, copy) NSString *image_url; // 图片
 @property (nonatomic, copy) NSString *s_image_url; // 小图
 @property (nonatomic, copy) NSString *deal_h5_url; // 链接
+
+//扩充详情界面需要显示的数据
+@property (nonatomic,copy)NSString *details;//团购详情
+@property (nonatomic,copy)NSString *notice;//重要通知
+@property (nonatomic,strong)DPRestrictions *restrictions;//约束
 @end

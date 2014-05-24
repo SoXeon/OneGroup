e8c6bd8e82edd6d7b356fd6ad88bef16d434bebb
SoXeon <dpsopower@gmail.com>
2014-05-24 10:05:07 +0800

    仅仅保留dealList页面上的独有属性

--- 一团/classes/deal/controller/DPDealListController.m
+++ 一团/classes/deal/controller/DPDealListController.m
@@ -27,8 +27,6 @@
 #define kItemW 250
 #define kItemH 250
 
-#define kDetailWitdh 600
-
 
 @interface DPDealListController () <DPRequestDelegate,MJRefreshBaseViewDelegate>
 {
@@ -36,20 +34,16 @@
     int _page;//页码
     MJRefreshFooterView *_footer;//下拉加载更多
     MJRefreshHeaderView *_header;
-    
-    DPCover *_cover;
+
 }
 
 @end
 
 @implementation DPDealListController
 
-- (id)init
+-(NSArray *)totalDeals
 {
-    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
-    layout.itemSize = CGSizeMake(kItemW, kItemH);
-    layout.minimumLineSpacing = 20;
-    return [self initWithCollectionViewLayout:layout];
+    return _deals;
 }
 
 - (void)viewDidLoad
@@ -124,11 +118,6 @@
     //监听城市改变的通知
     kAddAllNotes(dataChange)
     
-    //设置背景颜色
-    self.collectionView.backgroundColor = kGlobalBg;
-    [self.collectionView setDelegate:self];
-    [self.collectionView setDataSource:self];
-    
     //右边搜索框
     UISearchBar *s = [[UISearchBar alloc]init];
     s.frame = CGRectMake(0, 0, 210, 35);
@@ -140,19 +129,8 @@
     top.contentView = self.view;
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:top];
     
-    //注册cell的xib
-    [self.collectionView registerNib:[UINib nibWithNibName:@"DPDealCell" bundle:nil] forCellWithReuseIdentifier:@"deal"];
-    
-    //设置collectionView永远支持垂直滚动
-    self.collectionView.alwaysBounceVertical = YES;
-    
 }
 
--(void)viewWillAppear:(BOOL)animated
-{
-    //默认计算layout
-    [self didRotateFromInterfaceOrientation:0];
-}
 
 
 -(void)dataChange
@@ -167,96 +145,4 @@
     } error:nil];
 }
 
-#pragma mark 屏幕旋转的时候调用
-- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
-{
-    //取出layout
-    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *)self.collectionView.collectionViewLayout;
-    
-    //计算间距
-    CGFloat v = 20;
-    CGFloat h = 0;
-    
-    if (UIInterfaceOrientationIsLandscape(self.interfaceOrientation)) {
-      h = (self.view.frame.size.width - 3 * kItemW) / 4;
-    } else {
-      h = (self.view.frame.size.width - 2 * kItemW) / 3;
-    }
-    [UIView animateWithDuration:1.0 animations:^{
-        layout.sectionInset = UIEdgeInsetsMake(v, h, v, h);
-    }];
-}
-
--(void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
--(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    return _deals.count;
-}
-
--(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *ID = @"deal";
-    
-    DPDealCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ID forIndexPath:indexPath];
-    
-    cell.deal = _deals[indexPath.row];
-   
-    return cell;
-}
-
-#pragma mark -代理方法
--(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    [self showDetail:_deals[indexPath.row]];
-}
-
-#pragma mark 显示详情控制器
--(void)showDetail:(DPDeal *)deal
-{
-    if (_cover == nil) {
-        _cover = [DPCover coverWithTarget:self action:@selector(hideDetail)];}
-    _cover.frame = self.navigationController.view.bounds;
-    _cover.alpha = 0;
-    [UIView animateWithDuration:0.4 animations:^{
-        [_cover reset];
-    }];
-    [self.navigationController.view addSubview:_cover];
-    
-    //展示团购详情
-    DPDealDetailController *detail = [[DPDealDetailController alloc]init];
-    detail.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithIcon:@"btn_nac_close.png" highlightedIcon:@"btn_nav_close_hl.png" target:self action:@selector(hideDetail)];
-    detail.deal = deal;
-    DPNavigationController *nav = [[DPNavigationController alloc]initWithRootViewController:detail];
-    nav.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleLeftMargin;
-    nav.view.frame = CGRectMake(_cover.frame.size.width, 0, 600, _cover.frame.size.height);
-    //两个控制器为互为父子关系，view也是一样的 
-    [self.navigationController.view addSubview:nav.view];
-    [self.navigationController addChildViewController:nav];
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        CGRect f = nav.view.frame;
-        f.origin.x -= kDetailWitdh;
-        nav.view.frame = f;
-    }];
-}
-
--(void)hideDetail
-{
-    //隐藏遮盖
-    UIViewController *nav = [self.navigationController.childViewControllers lastObject];
-    [UIView animateWithDuration:0.3 animations:^{
-        _cover.alpha = 0;
-        CGRect f = nav.view.frame;
-        f.origin.x += kDetailWitdh;
-        nav.view.frame = f;
-    } completion:^(BOOL finished) {
-        [_cover removeFromSuperview];
-        [nav.view removeFromSuperview];
-        [nav removeFromParentViewController];
-    }];
-}
 @end

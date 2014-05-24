fa066f772d9f6fbdffb596266f2bd731a37825c6
SoXeon <dpsopower@gmail.com>
2014-05-24 10:04:31 +0800

    将大部分公用方法抽象到Base类中

--- 一团/classes/deal/controller/DPDealListController.h
+++ 一团/classes/deal/controller/DPDealListController.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2014年 戴鹏. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "DPBaseDealListController.h"
 
-@interface DPDealListController : UICollectionViewController 
+@interface DPDealListController : DPBaseDealListController 
 
 @end

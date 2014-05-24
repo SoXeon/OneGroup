b44a5c6e253e516efa2314edf6806acccf1e4508
SoXeon <dpsopower@gmail.com>
2014-05-24 23:37:10 +0800

    再封装一层，到DPDealShow类中

--- 一团/classes/other/controller/DPBaseDealListController.m
+++ 一团/classes/other/controller/DPBaseDealListController.m
@@ -9,34 +9,32 @@
 #import "DPBaseDealListController.h"
 #import "DPDeal.h"
 #import "DPDealCell.h"
-#import "UIBarButtonItem+DP.h"
-#import "DPDealDetailController.h"
-#import "DPNavigationController.h"
-#import "DPCover.h"
+
 
 #define kItemW 250
 #define kItemH 250
-#define kDetailWitdh 600
 
-@interface DPBaseDealListController ()
-{
-    DPCover *_cover;
-}
+@interface DPBaseDealListController () <UICollectionViewDataSource,UICollectionViewDelegate>
 
+
+@property (nonatomic,strong) UICollectionView *collectionView;
 @end
 
 @implementation DPBaseDealListController
 
-- (id)init
+
+-(void)viewDidLoad
 {
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
     layout.itemSize = CGSizeMake(kItemW, kItemH);
     layout.minimumLineSpacing = 20;
-    return [self initWithCollectionViewLayout:layout];
-}
-
--(void)viewDidLoad
-{
+    
+    self.collectionView = [[UICollectionView alloc]initWithFrame:self.view.bounds collectionViewLayout:layout];
+    self.collectionView.autoresizingMask = UIViewAutoresizingFlexibleHeight |UIViewAutoresizingFlexibleWidth;
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    [self.view addSubview:self.collectionView];
+    
     //注册cell的xib
     [self.collectionView registerNib:[UINib nibWithNibName:@"DPDealCell" bundle:nil] forCellWithReuseIdentifier:@"deal"];
 
@@ -83,51 +81,7 @@
     [self showDetail:self.totalDeals[indexPath.row]];
 }
 
-#pragma mark 显示详情控制器
--(void)showDetail:(DPDeal *)deal
-{
-    if (_cover == nil) {
-        _cover = [DPCover coverWithTarget:self action:@selector(hideDetail)];}
-    _cover.frame = self.navigationController.view.bounds;
-    _cover.alpha = 0;
-    [UIView animateWithDuration:0.4 animations:^{
-        [_cover reset];
-    }];
-    [self.navigationController.view addSubview:_cover];
-    
-    //展示团购详情
-    DPDealDetailController *detail = [[DPDealDetailController alloc]init];
-    detail.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithIcon:@"btn_nac_close.png" highlightedIcon:@"btn_nav_close_hl.png" target:self action:@selector(hideDetail)];
-    detail.deal = deal;
-    DPNavigationController *nav = [[DPNavigationController alloc]initWithRootViewController:detail];
-    nav.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleLeftMargin;
-    nav.view.frame = CGRectMake(_cover.frame.size.width, 0, 600, _cover.frame.size.height);
-    //两个控制器为互为父子关系，view也是一样的
-    [self.navigationController.view addSubview:nav.view];
-    [self.navigationController addChildViewController:nav];
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        CGRect f = nav.view.frame;
-        f.origin.x -= kDetailWitdh;
-        nav.view.frame = f;
-    }];
-}
 
--(void)hideDetail
-{
-    //隐藏遮盖
-    UIViewController *nav = [self.navigationController.childViewControllers lastObject];
-    [UIView animateWithDuration:0.3 animations:^{
-        _cover.alpha = 0;
-        CGRect f = nav.view.frame;
-        f.origin.x += kDetailWitdh;
-        nav.view.frame = f;
-    } completion:^(BOOL finished) {
-        [_cover removeFromSuperview];
-        [nav.view removeFromSuperview];
-        [nav removeFromParentViewController];
-    }];
-}
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
